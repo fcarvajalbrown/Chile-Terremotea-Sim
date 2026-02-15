5f9d5c5b95f98d4a1896cf058357e64ef6078a3d
Felipe Carvajal Brown <fcarvajalbrown@protonmail.com>
2026-02-15 14:39:56 -0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/chile-terremotea-sim/', // GitHub Pages base path
+  base: '/Chile-Terremotea-Sim/', // GitHub Pages base path
 })
